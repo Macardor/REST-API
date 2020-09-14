91c0f14461238ce3579c07dbf54f1a1d2be2b33c
Grelory <gmpwm99@gmail.com>
2020-09-14 13:49:27 +0200

    add templete to create db

--- src/main/java/com/codecool/restapi/App.java
+++ src/main/java/com/codecool/restapi/App.java
@@ -1,7 +1,22 @@
 package com.codecool.restapi;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import com.codecool.restapi.model.*;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Sample text");
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("restPU");
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction transaction = em.getTransaction();
+        transaction.begin();
+        Car car = new Car("Fiat", 5.5f, 1300f);
+        em.persist(car);
+        transaction.commit();
+        em.close();
+        emf.close();
     }
 }
